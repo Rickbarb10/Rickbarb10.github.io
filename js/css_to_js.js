9ccb4cfaa747c4ebb777f3720acbd6adb3bc8697
Riccardo Barberini <barberini.riccardo@gmail.com>
2023-02-03 09:40:35 +0100

    Aggiunte JS

---
+++ js/css_to_js.js
@@ -0,0 +1,5 @@
+const txtArea1 = document.querySelector('#textarea_1')
+const txtArea2 = document.querySelector('#textarea_2')
+const convertBtn = document.querySelector('button[data-trigger="css_to_js"]')
+
+console.log(txtArea1, txtArea2, convertBtn); // ANCHOR console.log
